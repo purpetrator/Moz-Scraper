51334f8ba224e23579f5acfdf7117523002f357e
Ana Chernov <anastasia.chernov@gmail.com>
2019-12-14 16:05:54 -0500

    fixing typo

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(express.static("public"));
 // Connect to MongoDB
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mozscraper";
 
-mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
+mongoose.connect(MONGODB_URI);
 
 // Set Handlebars.
 var exphbs = require("express-handlebars");
